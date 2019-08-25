a72cf11dec536d7a6f7c941b6fabaeca3fe9f75f
citychen <s0920832252s@gmail.com>
2019-08-25 12:13:06 +0800

    [ add ] DataTimeExtension
    * count the new WeekDay from now

---
+++ Essay/CityDateTimeExtension/CityDateTimeExtension/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityDateTimeExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.NextWeekDay(DayOfWeek.Sunday));
+            Console.ReadKey();
+        }
+    }
+
+    public static class CityDateTimeExtension
+    {
+        public static DateTime NextWeekDay(this DateTime from, DayOfWeek end)
+        {
+            int addDays = (int)end - (int)from.DayOfWeek;
+            return from.AddDays(addDays <= 0 ? addDays + 7 : addDays);
+        }
+    }
+}
